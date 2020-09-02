c0104927212e28a14df98c4dee67385cd90c0ad3
immanuel <immanuelr44@gmail.com>
2020-09-02 21:06:01 +0530

    Modified ota

--- src/org/pixelexperience/ota/misc/Constants.java
+++ src/org/pixelexperience/ota/misc/Constants.java
@@ -34,8 +34,8 @@ public final class Constants {
     static final String PROP_AB_DEVICE = "ro.build.ab_update";
     static final String PROP_DEVICE = "org.pixelexperience.device";
     static final String PROP_BUILD_VERSION = "org.pixelexperience.version";
-    static final String OTA_URL = "https://raw.githubusercontent.com/PixelExperience-FanEdition/OTA/master/builds/%s.json";
-    static final String DOWNLOAD_WEBPAGE_URL = "https://raw.githubusercontent.com/PixelExperience-FanEdition/OTA/master/changelogs/%s/%s.txt";
+    static final String OTA_URL = "https://raw.githubusercontent.com/PixelUnofficial-A2/OTA/master/builds/%s.json";
+    static final String DOWNLOAD_WEBPAGE_URL = "https://raw.githubusercontent.com/PixelUnofficial-A2/OTA/master/changelogs/%s/%s.txt";
     static final String PROP_VERSION_CODE = "org.pixelexperience.ota.version_code";
     static final String EXPORT_PATH = "PixelExperience-Updates/";
 }
